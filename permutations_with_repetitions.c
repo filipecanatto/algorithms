6dbb8b01939e470bf057fc92210446563a8baf03
wcanatto <wcanatto@cpqd.com.br>
2018-05-30 15:14:10 -0300

    ci009

--- permutations_with_repetitions.c
+++ permutations_with_repetitions.c
@@ -1,6 +1,6 @@
 /**
 
-FATEC - 01/2014 - ALP (Algoritimos e Logica de Programacao).
+FATEC - 01/2014.
 
 Faca um programa que liste todas as possiveis permutacoes de um conjunto de letras "i" informado pelo usuario.
 Essas combinacoes devem ter comprimento "g", esse dado tambem deve ser informado pelo usuario.
@@ -11,30 +11,39 @@ Essas combinacoes devem ter comprimento "g", esse dado tambem deve ser informado
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX 26
+
 int main() {
 	
 	// definition of variables
-	int *ptr;
-	
-	int group = 4;
-	char input[4] = {'a', 'b', 'c', 'd'};
-	int nmap[group+1] = {0, 0, 0, 0, 0};
-	
+	int group;
+	char input[MAX];
+	int *nmap;
 	int i;
 	int j;
 	int k;
-	int last_index = sizeof(input) - 1;
+	int last_index;
+	int input_size;
+	int count = 0;
 	
+	printf("Enter with the set os characters: ");
+	scanf("%s", input);
+	
+	printf("Enter with the lenght of the set: ");
+	scanf("%i",&group);
 	
 	// initialization of variables
-	ptr = &nmap[group];
+	input_size = strlen(input);
+	nmap = (int*) calloc (input_size, sizeof(int));
+	last_index = (input_size-1);
 	
 	while (nmap[0] == 0){
 		
 		// begin map cicle
-		for (i=0; i < group; i++){
+		for (i=0; i < input_size; i++){
 			nmap[group] = i;
-			
+			count ++;
+			printf ("%i - ", count);
 			for (j = 1; j <= group; j++){
 				printf ("%c", input[nmap[j]]);
 				//printf ("%i", nmap[j]);
@@ -53,7 +62,6 @@ int main() {
 					// exit the loop
 					break; 
 				}
-				
 			}
 		}	
 	}
